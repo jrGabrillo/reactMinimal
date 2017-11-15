81335a5678615473564ec4682ae5d00df103c767
Rufo Gabrillo <rufo.gabrillo@gmail.com>
2017-11-15 19:53:50 +0800

    Updated. App is now accepting JSX syntax. App have Start and Stop clock button;

--- www/js/process.js
+++ www/js/process.js
@@ -1,12 +1,24 @@
+let timer = 0;
 function clock() {
 	let dom = `${new Date().toLocaleTimeString()}`;
-	ReactDOM.render(dom, document.getElementById('root'));
-	setTimeout(function(){
-		clock();
-	},1000);
+	ReactDOM.render(dom, document.getElementById('clock'));
+
+	timer = setTimeout(function(){
+					clock();
+				},1000);
 }
 
+function stop(){
+	clearTimeout(timer);
+	console.log('x');
+}
+
+
 let _root = document.getElementById('root');
-let dom = <input type='button' onClick={clock} value='click me' />;
+let dom = <div>
+			<div id='clock'></div>
+				<input type='button' onClick={clock} value='Start Clock' />
+				<input type='button' onClick={stop} value='Stop Clock' />
+			</div>;
 
 ReactDOM.render(dom,_root);
