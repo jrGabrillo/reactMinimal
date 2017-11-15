6ad33dead6bf356c95887bd92442afcac8ab6378
Rufo Gabrillo <rufo.gabrillo@gmail.com>
2017-11-15 11:32:58 +0800

    Update

--- www/js/process.js
+++ www/js/process.js
@@ -1,13 +1,12 @@
-// function clock() {
-// 	let dom = `${new Date().toLocaleTimeString()}`;
-// 	ReactDOM.render(dom, document.getElementById('root'));
-// 	setTimeout(function(){
-// 		clock();
-// 	},1000);
-// }
+function clock() {
+	let dom = `${new Date().toLocaleTimeString()}`;
+	ReactDOM.render(dom, document.getElementById('root'));
+	setTimeout(function(){
+		clock();
+	},1000);
+}
 
-// clock();
 let _root = document.getElementById('root');
-let dom = `<input type='button' value='click me' />`;
+let dom = <input type='button' onClick={clock} value='click me' />;
 
 ReactDOM.render(dom,_root);
