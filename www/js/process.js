a33f301943249140064f03eda38c49ec2f78e0fb
Rufo Gabrillo <rufo.gabrillo@gmail.com>
2017-12-01 16:49:16 +0800

    Update;

--- www/js/process.js
+++ www/js/process.js
@@ -1,27 +1,46 @@
-var setTimer = 0;
-function clock(){
-	let timer = <h1>{new Date().toLocaleTimeString()}</h1>;
-	ReactDOM.render(timer,document.getElementById('timer'));	
-
-	setTimer = setTimeout(function(){
-		clock();
-	},1000);
-};
-
-function stopClock(){
-	clearInterval(setTimer);
+let process = {
+	menu:function(){
+		let content = <ul>
+			<li><a href='#' onClick={process.page1}>Menu 1</a></li>
+			<li><a href='#' onClick={process.page2}>Menu 2</a></li>
+			<li><a href='#' onClick={process.page3}>Menu 3</a></li>
+			<li><a href='#' onClick={process.page4}>Menu 4</a></li>
+		</ul>;
+		ReactDOM.render(content,document.getElementById('root'));
+	},
+	page1:function(){
+		let content = <div>
+				this is page 1<br/><a href='#' onClick={process.menu}>back</a>
+			</div>;
+			ReactDOM.render(content,document.getElementById('root'));
+	},
+	page2:function(){
+		let content = <div>
+				this is page 2<br/><a href='#' onClick={process.menu}>back</a>
+			</div>;
+			ReactDOM.render(content,document.getElementById('root'));
+	},
+	page3:function(){
+		let content = <div>
+				this is page 3<br/><a href='#' onClick={process.menu}>back</a>
+			</div>;
+			ReactDOM.render(content,document.getElementById('root'));
+	},
+	page4:function(){
+	let content = <div>
+				this is page 4<br/><a href='#' onClick={process.menu}>back</a>
+			</div>;
+			ReactDOM.render(content,document.getElementById('root'));
+	},
+	loading:function(){
+		let content = <div>
+					Loading...
+				</div>;
+		ReactDOM.render(content,document.getElementById('root'));
+	}
 }
 
-let btn = <div>
-				<div id='timer' className='text'></div>
-				<div id='execute' className='text'></div>
-				<div id='output' className='text'></div>
-				<div id='error' className='text'></div>
-				<div id='commands' className='text'></div>
-				<div id='dbfile' className='text'></div>
-				<div id='savedb' className='text'></div>
-				<button onClick={clock}>Start Clock</button>
-				<button onClick={stopClock}>Stop Clock</button>
-			</div>;
-
-ReactDOM.render(btn,document.getElementById('rangie'));
+process.loading();
+setTimeout(function(){
+	process.menu();
+},5000);
--- www/js/sqlite.js
+++ www/js/sqlite.js
@@ -20,23 +20,6 @@ let table = function(){
             let stmt = db.prepare(sql);
             return stmt.getAsObject({$start:1, $end:1});
         },
-        manipulate:function(){
-
-        }     
     }
 }();
 
-let dbase = db.ini();
-db.create(dbase,'tbl_admin',`id,name,address,email`);
-table.insert(dbase,`tbl_admin`,[1,'Rufo N. Gabrillo JR.','Macabito Calasiao Pangasinan','rufo.gabrillo@gmail.com']);
-
-let admin = table.view(dbase,`SELECT * FROM tbl_admin`);
-console.log(admin);
-
-let content = `
-Name: ${admin.name} <br/>
-Address: ${admin.address} <br/>
-Email: ${admin.email} <br/>
-`;
-
-document.getElementById('root').innerHTML = content;
